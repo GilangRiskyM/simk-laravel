ad0e0677732e72bb3eaf2355bfdd99439a36cf45
Gilang Risky Mahardika <gilangriskymahardhika@gmail.com>
2023-12-19 12:12:29 +0700

    edit head dan env

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Laravel 10 SIMK | @yield('title')</title>
+    <title>SIMK | @yield('title')</title>
     <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 10 | Login</title>
+    <title>SIMK | Login</title>
     <link rel="icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
